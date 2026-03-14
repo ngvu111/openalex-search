56ee490fa0d77a7fd5b3f6bbf2df77409fd1eeae
ngvu111 <vnbnhllr@gmail.com>
2026-03-14 22:30:36 +0200

    Update app.js

--- docs/app.js
+++ docs/app.js
@@ -79,7 +79,7 @@ function makeURL({ q, year, sourceType, per, sort, oa, hasFulltext, hasAbs, page
   params.set('select', [
     'id','doi','display_name','publication_year','cited_by_count',
     'open_access','has_fulltext','abstract_inverted_index',
-    'authorships.author.display_name',
+    'authorships',
     'primary_location.source.display_name',
     'primary_location.source.issn_l',
     'primary_location.source.type'
